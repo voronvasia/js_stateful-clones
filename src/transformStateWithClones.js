1f810da9135ddf16f58e51159985e8c1c2c9cd23
Vasyl Vorontsov <voronvasia@gmail.com>
2025-11-03 20:26:00 +0200

    task completed

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,28 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const arr = [];
+  let currentState = { ...state };
+
+  for (const action of actions) {
+    if (action.type === 'clear') {
+      currentState = {};
+    }
+
+    if (action.type === 'addProperties') {
+      currentState = Object.assign({ ...currentState }, action.extraData);
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete currentState[key];
+      }
+    }
+
+    arr.push({ ...currentState });
+  }
+
+  return arr;
 }
 
 module.exports = transformStateWithClones;
